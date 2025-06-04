5608d5bb9cfbed42893c31e92d729756bd257388
kayode7 <adelowokayode293@gmail.com>
2025-06-04 09:32:56 +0100

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    alert("Welcome to Kayode's website!");
+};
+document.getElementById("darkModeToggle").onclick = function() {
+    document.body.classList.toggle("dark-mode");
+};
